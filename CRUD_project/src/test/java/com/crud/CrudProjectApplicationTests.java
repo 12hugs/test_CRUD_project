bfbf1d01e0c1760445702fc9777c763bc64a72ec
진영준 <jinange9@gmail.com>
2023-11-29 19:09:56 +0900

    com
    com

--- CRUD_project/src/test/java/com/crud/CrudProjectApplicationTests.java
+++ CRUD_project/src/test/java/com/crud/CrudProjectApplicationTests.java
@@ -1,22 +1,38 @@
 package com.crud;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import com.crud.entity.Member;
-import com.crud.repository.MemberRepository;
+import com.crud.entity.Analysis;
+import com.crud.repository.AnalysisRepository;
+
+import lombok.RequiredArgsConstructor;
 
 @SpringBootTest
 class CrudProjectApplicationTests {
 
 	@Autowired
-	private MemberRepository memberRepository;
+	private AnalysisRepository analysisRepository;
 	
 	@Test
-	public void testMember() throws Exception {
+	public void db데이터_테스트() {
+		
+		for(int i=1; i<100; i++) {
+	        LocalDateTime now = LocalDateTime.now();
+	        DateTimeFormatter format = DateTimeFormatter.ofPattern("yyyy년 M월 d일 H시 m분");
+	        String formatDate = now.format(format);
+			
+			Analysis analysis = new Analysis();
+			analysis.setPredictionAccuracy(80L);
+			analysis.setProductImg(null);
+			analysis.setPredictionJdm("정상");
+			analysis.setPredictionDate(formatDate);
+			this.analysisRepository.save(analysis);
+		}
 		
 	}
 
